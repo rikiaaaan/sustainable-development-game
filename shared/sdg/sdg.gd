4fafb6ccfbfb9b0811a9fdeae5e9100553ceb5e2
rikiaaaan <106677202+ozopopo@users.noreply.github.com>
2023-10-11 22:23:12 +0900

    同じsdgsが触れるとアップグレードする機能を実装
    sdgsのCollisionをCollisionShape2DからCollisionPolygon2Dに変更

--- shared/sdg/sdg.gd
+++ shared/sdg/sdg.gd
@@ -1,41 +1,75 @@
 extends RigidBody2D
 
+signal touched_sdgs
+
 @export var print_shit:bool = false
 @export var phase:int = 0
 
 const PHASE_ZERO_SCALE:Vector2 = Vector2(0.2,0.2)
 
+
 func _init() -> void:
+
+
 	return
+
 
 func _ready() -> void:
+
 	set_polygon(146/2)
-	return
-	
-func set_phase() -> void:
-	phase = clampi(phase, 0, 16)
-	$Polygon2D/Sprite2D.region_rect = Rect2(9+161.2*(phase%6), 11+158*int(phase/6), 146, 146)
+
 	return
 
+
+func set_phase() -> void:
+
+	phase = clampi(phase, 0, 16)
+	$Polygon2D/Sprite2D.region_rect = Rect2(9+161.2*(phase%6), 11+158*int(phase/6), 146, 146)
+
+	return
+
+
 func set_polygon(size:float) -> void:
+	
 	var pv:PackedVector2Array
 	for i in range(0, 360, 4):
 		var v:Vector2 = Vector2.from_angle(deg_to_rad(i)) * size
 		pv.append(v)
 		pass
 	$Polygon2D.polygon = pv
+	$CollisionPolygon2D.polygon = pv
+
 	return
 
+
+func summon_sdg(pos:Vector2) -> void:
+
+
+
+	return
+
+
 func _physics_process(delta:float) -> void:
+
 	set_phase()
 	if position.y > 1000:
 		queue_free()
 		pass
 	
-	if print_shit:
-		for body in get_colliding_bodies():
-			print("get_collidingbody")
-			print(body.name)
+	for body in get_colliding_bodies():
+		if body.get("phase") == null:
+			continue
+		if body.phase == phase && !body.freeze && phase < 16:
+			freeze = true
+			body.freeze = true
+			body.collision_layer = 0
+			collision_layer = 0
+			
+			emit_signal("touched_sdgs", (position+body.position)/2, phase+1)
+			
+			body.queue_free()
+			queue_free()
 			pass
 		pass
+
 	return
--- test/test.gd
+++ test/test.gd
@@ -1,6 +1,7 @@
 extends Node
 
 func _input(event:InputEvent) -> void:
+
 	if event.is_action_pressed("fullscreen"):
 		if DisplayServer.window_get_mode() == DisplayServer.WINDOW_MODE_FULLSCREEN:
 			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
@@ -10,23 +11,35 @@ func _input(event:InputEvent) -> void:
 			pass
 		pass
 	if event.is_action_pressed("summon_sdg"):
-		summon_sdg()
+		summon_sdg(DisplayServer.mouse_get_position() - DisplayServer.window_get_position())
 		pass
+
 	return
 
-func summon_sdg() -> void:
+func summon_sdg(summon_pos:Vector2, phase:int=0) -> void:
+
 	var sdg = ResourceLoader.load("res://shared/sdg/sdg.tscn").instantiate()
-	var mouse_pos_rel:Vector2i = DisplayServer.mouse_get_position() - DisplayServer.window_get_position()
-	print(mouse_pos_rel)
-	sdg.position = mouse_pos_rel
+#	print(mouse_pos_rel)
+	sdg.position = summon_pos
+	sdg.phase = phase
+	sdg.connect("touched_sdgs", touched_sdgs)
 	$Node2D/Sdgs.add_child(sdg)
-	
+
+	return
+
+func touched_sdgs(summon_pos:Vector2, phase:int) -> void:
+
+	print(phase)
+	summon_sdg(summon_pos, phase)
+
 	return
 
 func _process(_delta:float) -> void:
+
 	var cam_vec:Vector2 = Vector2.ZERO
 	cam_vec.x = int(Input.is_key_pressed(KEY_L)) - int(Input.is_key_pressed(KEY_J))
 	cam_vec.y = int(Input.is_key_pressed(KEY_K)) - int(Input.is_key_pressed(KEY_I))
 	$Camera2D.position += cam_vec * 3
 	$Camera2D.zoom += Vector2(0.1,0.1) * (int(Input.is_key_pressed(KEY_E)) - int(Input.is_key_pressed(KEY_Q)))
+
 	return
