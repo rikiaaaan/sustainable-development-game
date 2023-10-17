ae9ebe292463bbbc3480caf6ede759f6d75dc3ea
rikiaaaan <106677202+ozopopo@users.noreply.github.com>
2023-10-17 20:02:57 +0900

    ゲームスタートボタンのテストを追加

--- title/title.gd
+++ title/title.gd
@@ -5,3 +5,17 @@ func _ready() -> void:
 
 
 	return
+
+
+func _on_texture_rect_mouse_entered() -> void:
+
+	$Ui/TextureRect.modulate = Color(1.0, 0.81, 0.06)
+
+	return
+
+
+func _on_texture_rect_mouse_exited() -> void:
+
+	$Ui/TextureRect.modulate = Color(1,1,1,1)
+
+	return
