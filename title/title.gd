f3cbbb7e450e7bb5b2ef9bbd43114a8b71df4788
rikiaaaan <106677202+ozopopo@users.noreply.github.com>
2023-10-22 11:32:09 +0900

    タイトル画面からそれぞれのシーンに移行する機能を追加

--- title/title.gd
+++ title/title.gd
@@ -2,20 +2,30 @@ extends Node
 
 func _ready() -> void:
 
+	
 
+	return
+
+func change_scene(scene_name:String, with_tween:bool=true) -> void:
+	get_node("../").change_scene(scene_name, with_tween)
+	return
+
+func _on_game_start_button_pressed() -> void:
+
+	change_scene("play")
 
 	return
 
 
-func _on_texture_rect_mouse_entered() -> void:
+func _on_my_score_button_pressed() -> void:
 
-	$Ui/TextureRect.modulate = Color(1.0, 0.81, 0.06)
+	change_scene("my_score")
 
 	return
 
 
-func _on_texture_rect_mouse_exited() -> void:
+func _on_how_to_play_button_pressed() -> void:
 
-	$Ui/TextureRect.modulate = Color(1,1,1,1)
+	change_scene("howtoplay")
 
 	return
