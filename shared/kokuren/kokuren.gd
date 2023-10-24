639e1894512dd41a31c4c598bdafda09156bc070
rikiaaaan <106677202+ozopopo@users.noreply.github.com>
2023-10-24 22:50:04 +0900

    国連が下の線をボールがすり抜けないように修正

--- shared/kokuren/kokuren.gd
+++ shared/kokuren/kokuren.gd
@@ -1,9 +1,10 @@
-extends Sprite2D
+extends Node2D
 
 @export var speed:float = 6.0
 
 const POLYGON_11_x:float = -140
 const POLYGON_01_x:float = -145
+
 
 func _ready() -> void:
 
@@ -14,7 +15,11 @@ func _ready() -> void:
 
 func set_raycast_line() -> void:
 
-	
+	var collision_point:Vector2 = $RayCast2D.get_collision_point()
+	$Label.text = var_to_str(collision_point)
+	$Label.text += "\n" + var_to_str($RayCast2D.get_collision_normal())
+	$Polygon2D.polygon[1].y = collision_point.y
+	$Polygon2D.polygon[2].y = collision_point.y
 
 	return
 
@@ -30,5 +35,6 @@ func _physics_process(_delta:float) -> void:
 
 	var move_vec:Vector2 = Vector2(int(Input.is_action_pressed("kokuren_move_right")) - int(Input.is_action_pressed("kokuren_move_left")),0)
 	position += move_vec * speed
+	set_raycast_line()
 
 	return
