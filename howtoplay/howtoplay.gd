23c9c9abc69b7ca72eb67cf69291d4bf6e7c2a54
rikiaaaan <106677202+ozopopo@users.noreply.github.com>
2023-10-28 19:25:14 +0900

    UIを作成

--- howtoplay/howtoplay.gd
+++ howtoplay/howtoplay.gd
@@ -1,1 +1,10 @@
 extends Node
+
+
+func _ready() -> void:
+
+	$Ui/PageController/Back/Button.connect("focus_entered", $Ui/PageController/Back/Button.release_focus)
+	$Ui/PageController/Forward/Button.connect("focus_entered", $Ui/PageController/Forward/Button.release_focus)
+
+	return
+
