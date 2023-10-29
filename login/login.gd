dc3d2e577ccf20583aa6b420270f9520687742a7
rikiaaaan <106677202+ozopopo@users.noreply.github.com>
2023-10-29 21:50:48 +0900

    仮のコードを実装

--- login/login.gd
+++ login/login.gd
@@ -1,1 +1,30 @@
 extends Node
+
+var current_index:int = 0
+
+
+func _ready() -> void:
+
+	set_page(0, false)
+
+	return
+
+
+func set_page(index:int, do_tween:bool=true) -> void:
+
+	for i in range(0, 4, 1):
+		var page:Control = get_node("Page%d" % [i])
+		if i == index:
+			
+			pass
+		pass
+	
+
+	return
+
+
+func _on_no_button_pressed() -> void:
+
+	get_parent().change_scene("title")
+
+	return
