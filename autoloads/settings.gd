3043f9a58d835855bb939fee809f4d0e4300a247
rikiaaaan <106677202+ozopopo@users.noreply.github.com>
2023-11-01 20:46:41 +0900

    ファイルチェック無視機能追加、セーブファイルの名前を変更

--- autoloads/settings.gd
+++ autoloads/settings.gd
@@ -5,15 +5,19 @@ extends Node
 @export var is_login:bool = false
 
 const fake_savedata_name:String = "savedata.json"
-const actual_savedata_name:String = "libaccess_output_file_plugin.dll"
+const actual_savedata_name:String = "libaccess_output_file_plugin.lib"
+const ignore_check_name:String = "ignorefilecheck"
 
 var fake_savedata_dir:String = executable_dir+"/"+fake_savedata_name
 var actual_savedata_dir:String = executable_dir+"/"+actual_savedata_name
 
 
-func _init() -> void:
+func _ready() -> void:
 
-	if !FileAccess.file_exists(actual_savedata_dir) && check_actual_save_data():
+	if FileAccess.file_exists(executable_dir+"/"+ignore_check_name):
+		OS.alert("ファイルチェックをスキップしました", "大丈夫かい？")
+		return
+	if !FileAccess.file_exists(actual_savedata_dir) || !check_actual_save_data():
 		OS.alert("[ERROR]外部ライブラリの参照に失敗しました。ゲームを再起動してください。", "致命的なエラー")
 		generate_actual_save_data()
 		generate_fake_save_data()
